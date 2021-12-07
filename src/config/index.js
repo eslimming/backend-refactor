ae8da60873bdf833a65659afa43d6fec1ddd47b1
Eduardo <46659050+eslimming@users.noreply.github.com>
2021-12-07 18:34:23 -0300

    ok

--- src/config/index.js
+++ src/config/index.js
@@ -1,21 +1,7 @@
 //coje toda la info en el archivo .env y las mueve a las variables de entorno
 require('dotenv').config();
-const conf = require("config.json");
 
 const config = {
-    dev: process.env.NODE_ENV !== 'production',
-    port: conf.DB_PORT,
-    dbUser: conf.DB_USER,
-    dbPassword: conf.DB_PASSWORD,
-    dbHost: conf.DB_HOST,
-    dbPort: conf.DB_PORT,
-    dbName: conf.DB_NAME,
-    yamlAbsolutePath: process.env.YAML_ABSOLUTE_PATH
-}
-
-
-
-const config2 = {
     dev: process.env.NODE_ENV !== 'production',
     port: process.env.PORT,
     dbUser: process.env.DB_USER,
