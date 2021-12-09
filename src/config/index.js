e5218dfac726491dc58526a07b90c681988f2ae2
Eduardo <46659050+eslimming@users.noreply.github.com>
2021-12-09 14:24:47 -0300

    ok

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,13 @@
 //coje toda la info en el archivo .env y las mueve a las variables de entorno
 require('dotenv').config();
+const postgresHost = process.env.paDockerHost ? process.env.paDockerHost : process.env.DB_HOST;
 
 const config = {
     dev: process.env.NODE_ENV !== 'production',
     port: process.env.PORT,
     dbUser: process.env.DB_USER,
     dbPassword: process.env.DB_PASSWORD,
-    dbHost: process.env.DB_HOST,
+    dbHost: postgresHost,
     dbPort: process.env.DB_PORT,
     dbName: process.env.DB_NAME,
     yamlAbsolutePath: process.env.YAML_ABSOLUTE_PATH
